98857e1c22dade4ac4b564aa63fe2c608860abee
andresssafunes <andressafunes92@gmail.com>
2025-11-11 11:48:56 -0300

    :tada: Commit Inicial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Home from "./pages/home/Home"
+
+
+function App() {
+  
+
+  return (
+    <> 
+{/* A <> Tag fantasma para limitação, sem ele não consegue retornar + de uma linha pode ser utilizado <div> também*/}
+    
+    <Home />
+    </>
+    
+  )
+}
+
+export default App
