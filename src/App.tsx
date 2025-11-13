acb35d0d3f6414cfbe9e87976c3accd23d4bba50
andresssafunes <andressafunes92@gmail.com>
2025-11-13 09:39:35 -0300

    :sparkles: Criação da Home Navibar e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Footer from "./componentes/footer/Footer"
+import Navibar from "./componentes/navbar/Navibar"
 import Home from "./pages/home/Home"
 
 
@@ -7,8 +9,9 @@ function App() {
   return (
     <> 
 {/* A <> Tag fantasma para limitação, sem ele não consegue retornar + de uma linha pode ser utilizado <div> também*/}
-    
+    <Navibar />
     <Home />
+    <Footer/>
     </>
     
   )
---
+++ src/componentes/navbar/Navibar.tsx
@@ -0,0 +1,27 @@
+function Navibar() {
+    return (
+
+        <>
+            <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
+                <div className="container flex justify-between text-lg mx-8">
+                   
+                   <p className="font-bold">
+                    Blog Pessoal</p> 
+
+                </div>
+                <div className="container flex justify-end mx-4">
+                    
+                    <p>Postagens
+                    Temas
+                    Cadastrar Temas
+                    Perfil
+                    Sair</p>         
+                
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default Navibar
